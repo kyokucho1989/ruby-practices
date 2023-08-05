c99d72f79a7941dc21b8176cee8b73eaf0d44c47
ko <kyokucho1989@gmail.com>
2023-08-06 06:27:02 +0900

    指摘事項修正

--- 04.ls/lsr.rb
+++ 04.ls/lsr.rb
@@ -2,28 +2,28 @@
 # frozen_string_literal: true
 
 require 'optparse'
-file_name_list = Dir.glob('*')
+file_names = Dir.glob('*')
 
-file_list = []
 display_col_size = 3
-fold_point = file_name_list.size / display_col_size - 1
-file_name_list.each do |name|
-  file_list << { name: }
+files = file_names.map do |name|
+  { name: }
 end
 
-display_col_size.times do
-  file_list << file_list.slice!(0..fold_point)
+fold_point = file_names.size / display_col_size
+
+matrix_type_files = []
+files.each_slice(fold_point) do |separated_files|
+  matrix_type_files << separated_files
 end
 
-num = fold_point - file_list.last.size + 1
+num = fold_point - matrix_type_files.last.size
 num.times do
-  file_list.last << { name: '' }
+  matrix_type_files.last << { name: '' }
 end
 
-disp_file_name_array = file_list.transpose
-
-disp_file_name_array.each do |file_array|
-  file_array.each do |file|
+disp_file_names = matrix_type_files.transpose
+disp_file_names.each do |disp_files|
+  disp_files.each do |file|
     print file[:name].ljust(40)
   end
   puts ''
