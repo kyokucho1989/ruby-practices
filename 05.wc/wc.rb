f26697043d821f58dfe9e5b9d2c4fc9991a8936b
ko <kyokucho1989@gmail.com>
2023-09-09 20:56:40 +0900

    オプションをひとつのハッシュにまとめて格納

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,11 +3,11 @@
 
 require 'optparse'
 
-def display_flag(has_c_option, has_l_option, has_w_option)
-  has_no_options = !(has_w_option || has_c_option || has_l_option)
-  is_display_bytes = has_c_option || has_no_options
-  is_display_lines = has_l_option || has_no_options
-  is_display_words = has_w_option || has_no_options
+def display_flag(options)
+  has_no_options = !(options[:has_c_option] || options[:has_l_option] || options[:has_w_option])
+  is_display_bytes = options[:has_c_option] || has_no_options
+  is_display_lines = options[:has_l_option] || has_no_options
+  is_display_words = options[:has_w_option] || has_no_options
   [is_display_bytes, is_display_lines, is_display_words]
 end
 
@@ -18,24 +18,24 @@ def compute_file_size(str)
   [row_size, word_size, byte_size]
 end
 
-def display_file_data(has_c_option, has_l_option, has_w_option, file_name = '', **size)
+def display_file_data(display_set, file_name = '', **size)
   row_size = size[:row_size]
   word_size = size[:word_size]
   byte_size = size[:byte_size]
   display = ''
-  display += row_size.to_s.rjust(8) if has_l_option
-  display += word_size.to_s.rjust(8) if has_w_option
-  display += byte_size.to_s.rjust(8) if has_c_option
+  display += row_size.to_s.rjust(8) if display_set[:is_display_lines]
+  display += word_size.to_s.rjust(8) if display_set[:is_display_words]
+  display += byte_size.to_s.rjust(8) if display_set[:is_display_bytes]
   display += " #{file_name}"
   puts display
 end
 
-def generate_file_data_one_line(is_display_bytes, is_display_lines, is_display_words, files)
+def generate_file_data_one_line(display_set, files)
   file_size_total = { row: 0, word: 0, byte: 0 }
   files.each do |file|
     str = File.read(file)
     row_size, word_size, byte_size = compute_file_size(str)
-    disp_file_data(is_display_bytes, is_display_lines, is_display_words, file, row_size:, word_size:, byte_size:)
+    display_file_data(display_set, file, row_size:, word_size:, byte_size:)
     file_size_total[:row] += row_size
     file_size_total[:word] += word_size
     file_size_total[:byte] += byte_size
@@ -43,30 +43,30 @@ def generate_file_data_one_line(is_display_bytes, is_display_lines, is_display_w
   row_size = file_size_total[:row]
   word_size = file_size_total[:word]
   byte_size = file_size_total[:byte]
-  disp_file_data(is_display_bytes, is_display_lines, is_display_words, 'total', row_size:, word_size:, byte_size:) if files.size > 1
+  display_file_data(display_set, 'total', row_size:, word_size:, byte_size:) if files.size > 1
 end
 
 def main
   opt = OptionParser.new
-  has_c_option = false
-  has_l_option = false
-  has_w_option = false
-  opt.on('-c') { |_flag| has_c_option = true }
-  opt.on('-l') { |_flag| has_l_option = true }
-  opt.on('-w') { |_flag| has_w_option = true }
+  options = { has_c_option: false, has_l_option: false, has_w_option: false}
+  opt.on('-c') { |_flag| options[:has_c_option] = true }
+  opt.on('-l') { |_flag| options[:has_l_option] = true }
+  opt.on('-w') { |_flag| options[:has_w_option] = true }
   opt.parse!(ARGV)
-  is_display_bytes, is_display_lines, is_display_words = display_flag(has_c_option, has_l_option, has_w_option)
+  
+  flags = display_flag(options)
+  display_set = {is_display_bytes: flags[0], is_display_lines: flags[1], is_display_words: flags[2]} 
   files = ARGV
 
   if files.size.positive?
-    generate_file_data_one_line(is_display_bytes, is_display_lines, is_display_words, files)
-
+    generate_file_data_one_line(display_set, files)
   else
     while (line = gets(nil))
       row_size, word_size, byte_size = compute_file_size(line)
-      display_file_data(is_display_bytes, is_display_bytes, is_display_words, row_size:, word_size:, byte_size:)
+      display_file_data(display_set, row_size:, word_size:, byte_size:)
     end
   end
+
 end
 
 main
