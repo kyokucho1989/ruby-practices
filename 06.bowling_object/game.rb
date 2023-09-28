b1a750c133fdb126e5aa6e4c6c073e5f8bd25edf
ko <kyokucho1989@gmail.com>
2023-09-29 05:13:52 +0900

    gameのmainでやっていた処理を初期化のなかに入れ込んだ

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -5,6 +5,10 @@ require_relative 'frame'
 
 class Game
   def initialize(shots)
+    shots = shots.map do |shot|
+      shot == 'X' ? shot : shot.to_i
+    end
+
     @frames = []
     while shots.size > 3
       frame = if shots[0] == 'X'
@@ -54,10 +58,6 @@ class Game
   end
 end
 
-argvs = ARGV.first.split(',')
-shots = argvs.map do |shot|
-  shot == 'X' ? shot : shot.to_i
-end
-
+shots = ARGV.first.split(',')
 game = Game.new(shots)
 puts game.total_score
