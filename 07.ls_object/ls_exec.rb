b1e0a2d453f408533dae1206f4e63e96cf41434c
ko <kyokucho1989@gmail.com>
2023-10-14 04:20:38 +0900

    ARGVを取得する場所を実行ファイルに変更

--- 07.ls_object/ls_exec.rb
+++ 07.ls_object/ls_exec.rb
@@ -6,6 +6,13 @@ require_relative 'ls_option'
 require_relative 'ls_file'
 require_relative 'ls_command'
 
-option = LsOption.new
+opt = OptionParser.new
+option_existence = { a_option: false, r_option: false, l_option: false }
+opt.on('-a') { |_flag| option_existence[:a_option] = true }
+opt.on('-r') { |_flag| option_existence[:r_option] = true }
+opt.on('-l') { |_flag| option_existence[:l_option] = true }
+opt.parse!(ARGV)
+
+option = LsOption.new(option_existence)
 ls = LsCommand.new(option)
 ls.display_file_data(option)
--- 07.ls_object/ls_option.rb
+++ 07.ls_object/ls_option.rb
@@ -3,14 +3,17 @@
 class LsOption
   attr_reader :a_option, :l_option, :r_option
 
-  def initialize
-    opt = OptionParser.new
-    @a_option = false
-    @r_option = false
-    @l_option = false
-    opt.on('-a') { |_flag| @a_option = true }
-    opt.on('-r') { |_flag| @r_option = true }
-    opt.on('-l') { |_flag| @l_option = true }
-    opt.parse!(ARGV)
+  def initialize(options)
+    @a_option = options[:a_option]
+    @r_option = options[:r_option]
+    @l_option = options[:l_option]
+    # opt = OptionParser.new
+    # @a_option = false
+    # @r_option = false
+    # @l_option = false
+    # opt.on('-a') { |_flag| @a_option = true }
+    # opt.on('-r') { |_flag| @r_option = true }
+    # opt.on('-l') { |_flag| @l_option = true }
+    # opt.parse!(ARGV)
   end
 end
